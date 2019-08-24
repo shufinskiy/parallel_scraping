a534fe078904cb578737b23dc65c1e27d100de3d
Vladislav <hollywood.90210@mail.ru>
2019-08-24 18:21:24 +0300

    Update Rcrawler.R
    Adding comments

--- Rcrawler.R
+++ Rcrawler.R
@@ -1,8 +1,19 @@
 library(Rcrawler)
 library(stringr)
 
+# Получение 100 адресов страниц книжного магазина Лабиринт
 list_url <- paste0("https://www.labirint.ru/books/", 
                    str_pad(c(701601:701700), 7, side = "left", pad = 0), "/")
+
+# Scraping 100 страниц Лабиринта с помощью функции ContentScraper пакета  Rcrawler.
+# system.time - функция, возвращающая время выполнения выражения.
+
+# Аргументы функции ContentScraper:
+# CssPatterns - один или несколько CSS шаблонов для извлечения данных.
+# ExcludeCSSPat - один или несколько CSS шаблонов, которые не нужно извлекать.
+# Полезно, когда нужный CSS содержит в себе ещё CSS элементы, которые нам не нужны.
+# ManyPerPattern - если FALSE, то извлекается только первый элемент со страницы,
+# подходящий шаблону. Если TRUE, то все элементы со страницы, подходящие по шаблону.
 
 sys <- system.time(t <- ContentScraper(list_url, CssPatterns = c("#product-title", 
                                                                  ".authors", 
