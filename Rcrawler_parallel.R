a3b53da718a1a571d2d2ae1d3059b0bb60a54b96
Vladislav <hollywood.90210@mail.ru>
2019-08-24 22:27:47 +0300

    Update Rcrawler_parallel.R
    Adding comments

--- Rcrawler_parallel.R
+++ Rcrawler_parallel.R
@@ -1,10 +1,34 @@
+# Загружаемый пакет для параллельного вычисления
 library(doParallel)
+
+# Scraping 100 страниц Лабиринта с помощью функции ContentScraper пакета  Rcrawler.
+# system.time - функция, возвращающая время выполнения выражения.
+
 sys <- system.time({
+  
+  # Получение 100 адресов страниц книжного магазина Лабиринт 
   list_url <- paste0("https://www.labirint.ru/books/", 
                      str_pad(c(701601:701700), 7, side = "left", pad = 0), "/")
+  
+  # detectCores - функция, считающая количество ядер процессора
   number_cl <- detectCores()
+  
+  # makePSOCKcluster - функция создающая копии R, которые работают параллельно 
   cluster <- makePSOCKcluster(number_cl)
+  
   registerDoParallel(cluster)
+
+  # Аргументы функции ContentScraper:
+  # CssPatterns - один или несколько CSS шаблонов для извлечения данных.
+  
+  # ExcludeCSSPat - один или несколько CSS шаблонов, которые не нужно извлекать.
+  # Полезно, когда нужный CSS содержит в себе ещё CSS элементы, которые нам не нужны.
+  
+  # ManyPerPattern - если FALSE, то извлекается только первый элемент со страницы,
+  # подходящий шаблону. Если TRUE, то все элементы со страницы, подходящие по шаблону.
+  
+  # PatternsName - имена для каждого извлеченного элемента страницы. Нужно для
+  # дальнейшего преобразования спика в таблицу, когда элементы списка неодинаковой длины
   
   t_func <- function(n){
     library(Rcrawler)
@@ -27,6 +51,13 @@ sys <- system.time({
                                          "page"))
     return(t)
   }
+  
+  # parLapply - аналог lapply функции для параллельных вычислений
   big_list12 <- parLapply(cluster, list_url, t_func)
+  
+  # Прекращение параллельных вычислений
   stopCluster(cluster)
 })
+
+# Создание таблицы данных из списка
+f <- data.table::rbindlist(big_list12, fill = TRUE)
